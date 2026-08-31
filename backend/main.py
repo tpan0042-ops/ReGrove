c0aafa9bedbc44e9dafe1b7178896a3e0d253437
Tao Pan <tpan0042@student.monash.edu>
2026-08-31 18:13:50 +1000

    Connect FastAPI to PostgreSQL

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,48 @@
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "FastAPI backend is running"}
+
+
+@app.get("/plants")
+def get_plants():
+    # Connect to the local PostgreSQL database.
+    connection = psycopg2.connect(
+    host="localhost",
+    port="5433",
+    database="regrove",
+    user="regrove",
+    password="regrove_local"
+    )
+
+    # Read the sample plant data.
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT plant_species_id, scientific_name, common_name, native_status
+        FROM plant_species
+    """)
+
+    rows = cursor.fetchall()
+
+    plants = []
+
+    for row in rows:
+        plant = {
+            "plant_species_id": row[0],
+            "scientific_name": row[1],
+            "common_name": row[2],
+            "native_status": row[3]
+        }
+
+        plants.append(plant)
+
+    cursor.close()
+    connection.close()
+
+    return plants
